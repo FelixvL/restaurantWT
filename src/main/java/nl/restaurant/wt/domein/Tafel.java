e5bcc4c0fa526cb5387978e98200bc93a6ea8632
FelixvL <digitaleservice@gmail.com>
2020-07-07 10:51:41 +0200

    tafel toegevoegd

---
+++ src/main/java/nl/restaurant/wt/domein/Tafel.java
@@ -0,0 +1,14 @@
+package nl.restaurant.wt.domein;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tafel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	String tafelnummer;
+}
